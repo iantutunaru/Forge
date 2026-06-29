ff64a71c0418809b2c0627e8d9eb4dbcc662a383
Ianis Tutunaru <129986472+iantutunaru@users.noreply.github.com>
2026-06-29 20:49:38 +0300

    Add Chunk spawning and import running animation
    * Add Generic_Characters@Running.fbx and import metadata under Assets/Imports/Animations.
    
    * Update Assets/Animation/Runner.controller to include a Running state (references the new clip) and set it as the default state in the Base Layer.
    
    * Adjust Runner chunk prefab local Y position to -0.5 to align with the scene geometry.
    
    * Update the Runner demo scene to reflect these changes and include related asset references.
    
    * Implement base of LevelGenerator.cs. Now creates a set amount of chunks from the selected base.

--- Assets/Scripts/Runner/LevelGenerator.cs
+++ Assets/Scripts/Runner/LevelGenerator.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 
-public class LevelGenerator : MonoBehaviour
+namespace Runner
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class LevelGenerator : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private GameObject chunkPrefab;
+        [SerializeField] private int startingChunksAmount = 12;
+        [SerializeField] private Transform chunksParent;
+        [SerializeField] private float chunkLength = 10f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Start()
+        {
+            for (int i = 0; i < startingChunksAmount; i++)
+            {
+                float spawnPositionZ;
+
+                if (i == 0)
+                {
+                    spawnPositionZ = transform.position.z;
+                }
+                else
+                {
+                    spawnPositionZ = transform.position.z + (i * chunkLength);
+                }
+                
+                Vector3 chunkSpawnPosition = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
+                
+                Instantiate(chunkPrefab, chunkSpawnPosition, Quaternion.identity, chunksParent);
+            }
+        }
     }
 }
